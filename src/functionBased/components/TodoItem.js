297b9c4abad29299302e0078f83f5a16c17e138f
Andreas Lord <andreas.lord@climatepartner.com>
2022-02-15 14:03:02 +0100

    Added icons (and styled them) to the buttons in the app.

--- src/functionBased/components/TodoItem.js
+++ src/functionBased/components/TodoItem.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-
+import { FaTrash } from "react-icons/fa"
 import styles from "./TodoItem.module.css"
 
 const TodoItem = props => {
@@ -52,7 +52,11 @@ const TodoItem = props => {
           checked={completed}
           onChange={() => props.handleChangeProps(id)}
         />
-        <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
+        <button onClick={() => props.deleteTodoProps(id)}>
+          <FaTrash
+            style={{ color: "orangered", fontSize: "18px" }}
+          />
+        </button>
         <span style={completed ? completedStyle : null}>{title}</span>
       </div>
       <input
